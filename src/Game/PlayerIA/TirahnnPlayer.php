fdcf2b3f705429219edc79bbea15be8b3b364d02
Davin <clement.davin@epita.fr>
2020-10-07 19:50:24 +0200

    only paper

--- src/Game/PlayerIA/TirahnnPlayer.php
+++ src/Game/PlayerIA/TirahnnPlayer.php
@@ -42,7 +42,7 @@ class TirahnnPlayer extends Player
         // -------------------------------------    -----------------------------------------------------
 
 
-        return parent::rockChoice();
+        return parent::paperChoice();
 
     }
 };
